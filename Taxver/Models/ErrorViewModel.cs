1520c9aa67e76b7de016d565836eb9b04049be16
aterron71 <aterron71@github.com>
2018-09-17 21:49:43 -0500

    Agregar archivos de proyecto.

---
+++ Taxver/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Taxver.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
